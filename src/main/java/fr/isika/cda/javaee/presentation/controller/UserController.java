848c3cc4622394f465fb819c8d7cef363fbe6cb2
Thibault231 <salgues231@gmail.com>
2024-01-02 21:16:15 +0100

    Add Authenticate and logout method in userControler

--- src/main/java/fr/isika/cda/javaee/presentation/controller/UserController.java
+++ src/main/java/fr/isika/cda/javaee/presentation/controller/UserController.java
@@ -1,7 +1,12 @@
 package fr.isika.cda.javaee.presentation.controller;
 
+import java.util.List;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
 import javax.inject.Inject;
 import javax.inject.Named;
+import javax.servlet.http.HttpSession;
 
 import fr.isika.cda.javaee.dao.IDaoUser;
 import fr.isika.cda.javaee.entity.users.User;
@@ -14,7 +19,15 @@ public class UserController {
 	private IDaoUser userDao;
 	private UserViewModel userViewModel;
 
-	public String addNewUser() {
+	public UserViewModel getUserViewModel() {
+		return userViewModel;
+	}
+
+	public void setUserViewModel(UserViewModel userViewModel) {
+		this.userViewModel = userViewModel;
+	}
+
+	public String createUser() {
 		User userToCreate = new User();
 		userToCreate.setActive(true);
 		userDao.createUser(userToCreate);
@@ -23,6 +36,41 @@ public class UserController {
 
 	public String deleteUser(Long userToDeleteId) {
 		userDao.deleteUser(userToDeleteId);
+		return "index";
+	}
+
+	public List<User> getAllActiveUser() {
+		return null;
+	}
+
+	public User getUser(Long userId) {
+		return null;
+	}
+
+	public User getUser(String userEmail) {
+		return null;
+	}
+
+	public String authenticate() {
+		String message;
+		FacesContext fc = FacesContext.getCurrentInstance();
+		if (userViewModel.getEmail().isEmpty()) {
+			message = "Login " + userViewModel.getEmail() + " inexistant !!";
+			fc.addMessage(null, new FacesMessage(message));
+			return null;
+		} else if (userViewModel.getPassword().isEmpty()) {
+			message = "vérifiez votre mot de passe ";
+			fc.addMessage(null, new FacesMessage(message));
+			return null;
+		} else {
+			fc.getExternalContext().getSessionMap().put("login", userViewModel.getEmail());
+		}
+		return "Page2";
+	}
+
+	public String logout() {
+		HttpSession session = (HttpSession) FacesContext.getCurrentInstance().getExternalContext().getSession(false);
+		session.invalidate();
 		return "index";
 	}
 
