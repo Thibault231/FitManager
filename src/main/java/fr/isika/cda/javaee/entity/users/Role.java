e18eb8b5bb97611ec66d0d849046c670f40519f2
Thibault231 <salgues231@gmail.com>
2024-01-04 12:26:55 +0100

    Clean enum

--- src/main/java/fr/isika/cda/javaee/entity/users/Role.java
+++ src/main/java/fr/isika/cda/javaee/entity/users/Role.java
@@ -1,22 +1,7 @@
 package fr.isika.cda.javaee.entity.users;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-
-@Entity
 public enum Role {
 
-	SuperAdmin(1), Gestionnaire(2), Coach(3), Adherent(4), Visiteur(5);
+	SuperAdmin, Gestionnaire, Coach, Adherent, Visiteur;
 
-	@Id
-	private final int code;
-
-	private Role(int code) {
-		this.code = code;
-	}
-
-	public int getCode() {
-		return code;
-	}
-	
 }
--- src/main/java/fr/isika/cda/javaee/entity/users/Type.java
+++ src/main/java/fr/isika/cda/javaee/entity/users/Type.java
@@ -1,21 +1,7 @@
 package fr.isika.cda.javaee.entity.users;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-
-@Entity
 public enum Type {
 
-	Diploma(1), Id(2), MedicalCertificate(3), BankDetails(4);
+	Diploma, Id, MedicalCertificate, BankDetails;
 
-	@Id
-	private final int code;
-
-	private Type(int code) {
-		this.code = code;
-	}
-
-	public int getCode() {
-		return code;
-	}
 }
