c73730937e0e32d55f958853a5e944d29adf7794
Thibault231 <salgues231@gmail.com>
2024-01-05 11:31:28 +0100

    Add schedulde class

--- src/main/java/fr/isika/cda/javaee/entity/relations/Schedulde.java
+++ src/main/java/fr/isika/cda/javaee/entity/relations/Schedulde.java
@@ -5,6 +5,7 @@ import java.util.List;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 
 @Entity
 public class Schedulde {
@@ -13,5 +14,23 @@ public class Schedulde {
 	@GeneratedValue
 	private Long scheduldeId;
 
+	@OneToMany
 	private List<Booking> bookingsList;
+
+	public Long getScheduldeId() {
+		return scheduldeId;
+	}
+
+	public void setScheduldeId(Long scheduldeId) {
+		this.scheduldeId = scheduldeId;
+	}
+
+	public List<Booking> getBookingsList() {
+		return bookingsList;
+	}
+
+	public void setBookingsList(List<Booking> bookingsList) {
+		this.bookingsList = bookingsList;
+	}
+
 }
