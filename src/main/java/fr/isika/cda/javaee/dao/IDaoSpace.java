e48a3f81323b1eac699915252a2848017efa16f9
Thibault231 <salgues231@gmail.com>
2024-01-05 09:16:53 +0100

    Add coments for ISpaceDao Methods

--- src/main/java/fr/isika/cda/javaee/dao/IDaoSpace.java
+++ src/main/java/fr/isika/cda/javaee/dao/IDaoSpace.java
@@ -8,19 +8,44 @@ import fr.isika.cda.javaee.presentation.viewmodel.SpaceViewModel;
 public interface IDaoSpace {
 
 	/**
+	 * Add a new Space in the database.
 	 * 
-	 * @param space
-	 * @return
+	 * @param spaceToCreate (:Space)
+	 * @return Created space Id(:Long)
 	 */
 	Long createSpace(Space spaceToCreate);
 
-	boolean deleteSpace(Long SpaceToDeleteIdSubscription);
+	/**
+	 * Hard delete of a space from the Db, using it's id.
+	 * 
+	 * @param SpaceToDeleteId
+	 * @return True if success, false otherwise (: boolean)
+	 */
+	boolean deleteSpace(Long SpaceToDeleteId);
 
-	Space getSpaceByIdSubscription(Long spaceToGetIdSubscription);
+	/**
+	 * Return a Space from the database, using it's Id, if it exist or null
+	 * otherwise.
+	 * 
+	 * @param spaceToGetId
+	 * @return the space to get (:Space)
+	 */
+	Space getSpaceById(Long spaceToGetId);
 
+	/**
+	 * Return a Space from the database, using it's FitnessName, if it exist or null
+	 * otherwise.
+	 * 
+	 * @param spaceToGetName (:String)
+	 * @return the space to get (:Space)
+	 */
 	Space getSpaceByName(String spaceToGetName);
 
+	/**
+	 * Return the list of all Spaces of the plateform.
+	 * 
+	 * @return the list of spaces to get (:Lis<Space>)
+	 */
 	List<Space> getAllSpace();
 
-	Space getSpaceById(Long spaceId);
 }
