51189d94135aa3c1d678654505a3348c7e2124af
Charef31 <charef.senouci31@gmail.com>
2024-01-09 17:37:35 +0100

    on peut presque réserver!!

--- src/main/java/fr/isika/cda/javaee/presentation/controller/BookingController.java
+++ src/main/java/fr/isika/cda/javaee/presentation/controller/BookingController.java
@@ -6,9 +6,12 @@ import javax.faces.view.ViewScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
 
+import fr.isika.cda.javaee.dao.IDaoCourse;
+import fr.isika.cda.javaee.dao.IDaoSpace;
 import fr.isika.cda.javaee.dao.IDaoUser;
 import fr.isika.cda.javaee.dao.relations.BookingDao;
 import fr.isika.cda.javaee.dao.relations.IBookingDao;
+import fr.isika.cda.javaee.entity.plateform.Course;
 import fr.isika.cda.javaee.entity.relations.Booking;
 import fr.isika.cda.javaee.entity.users.User;
 import fr.isika.cda.javaee.presentation.util.SessionUtils;
@@ -27,22 +30,34 @@ public class BookingController implements Serializable {
 	private IDaoUser userDao;
 	
 	@Inject
+	private IDaoCourse courseDao;
+
+	
+	@Inject
 	private CourseController courseController;
 	
 	private BookingForm bookingForm = new BookingForm();
 
 	public void createBooking() {
-
-		Booking bookingToCreate = new Booking();
-
+		Booking bookingToCreate = new Booking(true);
 		iBookingDao.createBooking(bookingToCreate);
 	}
 	
 	public void reserveCourse() {
+		//1-On récupérer l'adherent et on le lie à la réservation
 		User currentUser = userDao.getUserById(SessionUtils.getUserIdFromSession());
+		Booking bookingToCreate = new Booking(true);
+		bookingToCreate.setMember(currentUser);
+		//2-On récupère le cours et on le lie à la réservation
+		Long courseId =  Long.valueOf(courseController.getEvent().getId());
+		Course currentCourse = courseDao.getCourseById(courseId);
+		bookingToCreate.setLinkedCourse(currentCourse);
 		
-		String courseName = courseController.getEvent().getTitle();
-		String courseDescritpion = courseController.getEvent().getTitle();
+		//3-On persiste la réservation
+		iBookingDao.createBooking(bookingToCreate);
+		
+		
+		
 		
 		courseController.resetEvent();
 	}
