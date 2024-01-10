557e8dd3e7467def9345a90b9910000a67475d99
Thibault231 <salgues231@gmail.com>
2024-01-11 00:42:32 +0100

    fix coach course listing

--- src/main/java/fr/isika/cda/javaee/presentation/controller/SpaceController.java
+++ src/main/java/fr/isika/cda/javaee/presentation/controller/SpaceController.java
@@ -62,104 +62,6 @@ public class SpaceController implements Serializable {
 
 //**********************************************************
 	/**
-	 * Authenticate a visitor from a login form and create a session object with
-	 * role and id. The authenticated visitor is next redirected to it's specific
-	 * dashboard. <br/>
-	 * <b>Use this method for space only</b>
-	 * 
-	 * @return url (:String)
-	 */
-	public String authenticateOnSpace() {
-		String message;
-		FacesContext fc = FacesContext.getCurrentInstance();
-		Account userToLogAccount = spaceViewModel.getUser().getAccount();
-		if (userToLogAccount.getLogin().isEmpty()) {
-			message = "Login inexistant !!";
-			fc.addMessage(null, new FacesMessage(message));
-			return "SpaceLoginForm?faces-redirect=true";
-		} else if (userToLogAccount.getPassword().isEmpty()) {
-			message = "vérifiez votre mot de passe ";
-			fc.addMessage(null, new FacesMessage(message));
-			return "SpaceLoginForm?faces-redirect=true";
-		} else {
-			User userToLog = this.userDao.getUserBySpaceAndLogin(userToLogAccount.getLogin(),
-					SessionUtils.getSpaceIdFromSession());
-			if (userToLog != null && userToLog.getAccount().getPassword().equals(userToLogAccount.getPassword())) {
-				fc.getExternalContext().getSessionMap().put("role", userToLog.getAccount().getRole());
-				fc.getExternalContext().getSessionMap().put("id", userToLog.getUserId());
-				fc.getExternalContext().getSessionMap().put("name", userToLog.getProfile().getCivility().getName());
-				return redirectToRightDashBoard(userToLog.getAccount().getRole());
-			} else {
-				message = "Mot de passe erroné. ";
-				fc.addMessage(null, new FacesMessage(message));
-				return "SpaceLoginForm?faces-redirect=true";
-			}
-		}
-	}
-
-	/**
-	 * Get the Creating coach form using the UserviewModel, then call the
-	 * UserService to create a new user.<br/>
-	 * <b>Use this method for creating only coach</b>
-	 * 
-	 * @return url (:String)
-	 */
-	public String createCoachAccount() {
-		// Injecte le role et le mot de passe par défaut dans le formulaire
-		this.spaceViewModel.getNewUser().getAccount().setRole(Role.Coach);
-		this.spaceViewModel.getNewUser().getAccount().setPassword("00000");
-		try {
-			// Attrappe la salle courante
-			Long currentSpaceId = SessionUtils.getSpaceIdFromSession();
-			Space currentSpace = spaceDao.getSpaceWithMembers(currentSpaceId);
-			// Persist le nouveau coach
-			Long createdUserId = userSvc.createUser(spaceViewModel.getNewUser());
-			// Lie le coach et la salle courante
-			User createdUser = userDao.getUserByIdWithLinkedSpaces(createdUserId);
-			currentSpace.getUsers().add(createdUser);
-			createdUser.getLinkedSpaces().add(currentSpace);
-			spaceDao.updateSpace(currentSpace);
-			userDao.updateUser(createdUser);
-			// Réinitialise le formulaire du viewmodel
-			this.spaceViewModel.setNewUser(new User(true));
-			return "ManagerSpaceDashBoard";
-		} catch (UserExistsException e) {
-			System.out.println("Exception : " + e.getMessage());
-			this.spaceViewModel.setNewUser(new User(true));
-			return "RegisterCoachForm?faces-redirect=true";
-		}
-	}
-
-	/**
-	 * Get the Creating member form using the UserviewModel, then call the
-	 * UserService to create a new user.<br/>
-	 * <b>Use this method for creating only adherent</b>
-	 * 
-	 * @return url (:String)
-	 */
-	public String createMemberAccount() {
-		Long currentSpaceId = SessionUtils.getSpaceIdFromSession();
-
-		Space currentSpace = spaceDao.getSpaceWithMembers(currentSpaceId);
-		this.spaceViewModel.getUser().getAccount().setRole(Role.Adherent);
-
-		try {
-			Long createdUserId = userSvc.createUser(spaceViewModel.getUser());
-			User createdUser = userDao.getUserByIdWithLinkedSpaces(createdUserId);
-			currentSpace.getUsers().add(createdUser);
-			createdUser.getLinkedSpaces().add(currentSpace);
-
-			spaceDao.updateSpace(currentSpace);
-			userDao.updateUser(createdUser);
-
-			return authenticateOnSpace();
-		} catch (UserExistsException e) {
-			System.out.println("Exception : " + e.getMessage());
-			return "RegisterMemberForm?faces-redirect=true";
-		}
-	}
-
-	/**
 	 * Create a space and link it to it's manager creator.
 	 * 
 	 * @return url (String)
@@ -202,17 +104,6 @@ public class SpaceController implements Serializable {
 		spaceDao.deleteSpace(spaceToDeleteId);
 
 		return "index?faces-redirect=true";
-	}
-
-	public void uploadAdministrativeDocument(FileUploadEvent event) throws Exception {
-		String timestamp = LocalDateTime.now().format(DateTimeFormatter.ofPattern("ddMMyyyyHHmmss"));
-
-		UploadedFile uploadedFile = event.getFile();
-		String fileName = String.join("_", timestamp, uploadedFile.getFileName());
-
-		spaceViewModel.getUser().getAccount().getAdministrativeDocument().setFilePath(fileName);
-
-		FileUploadUtils.uploadFileToApp(uploadedFile, fileName);
 	}
 
 	/**
@@ -328,19 +219,4 @@ public class SpaceController implements Serializable {
 		this.uploadedFile = uploadedFile;
 	}
 
-	public String updateUser() {
-		// mettre à jour le user
-		userSvc.updateUserOnPlateform(spaceViewModel.getUser(), SessionUtils.getUserIdFromSession());
-		// mettre à jour la session si le nom est changé
-		if (spaceViewModel.getUser().getProfile().getCivility().getName() != null) {
-			String newName = spaceViewModel.getUser().getProfile().getCivility().getName();
-			FacesContext fc = FacesContext.getCurrentInstance();
-			fc.getExternalContext().getSessionMap().put("name", newName);
-		}
-		return redirectToRightDashBoard(SessionUtils.getUserRoleFromSession());
-	}
-
-	public UploadedFile getUploadedFile() {
-		return uploadedFile;
-	}
 }
