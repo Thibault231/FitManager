760d1838b7cf04445c5f1807539babd1b9481cf8
AlexandraAKA <141423433+AlexandraAKA@users.noreply.github.com>
2024-01-10 17:23:48 +0100

    Methode update ok

--- src/main/java/fr/isika/cda/javaee/services/UserServices.java
+++ src/main/java/fr/isika/cda/javaee/services/UserServices.java
@@ -1,9 +1,12 @@
 package fr.isika.cda.javaee.services;
 
+import java.util.Date;
+
 import javax.ejb.Stateless;
 import javax.inject.Inject;
 
 import fr.isika.cda.javaee.dao.IDaoUser;
+import fr.isika.cda.javaee.entity.users.Sex;
 import fr.isika.cda.javaee.entity.users.User;
 import fr.isika.cda.javaee.exceptions.UserExistsException;
 
@@ -40,7 +43,42 @@ public class UserServices {
 			String newName = userToUpdate.getProfile().getCivility().getName();
 			currentUser.getProfile().getCivility().setName(newName);
 		}
-		userDao.updateUser(currentUser);
+
+		if (userToUpdate.getProfile().getCivility().getForename() != null) {
+			String newForename = userToUpdate.getProfile().getCivility().getForename();
+			currentUser.getProfile().getCivility().setForename(newForename);
+		}
+
+		if (userToUpdate.getProfile().getCivility().getSex() != null) {
+			Sex newSex = userToUpdate.getProfile().getCivility().getSex();
+			currentUser.getProfile().getCivility().setSex(newSex);
+		}
+
+		if (userToUpdate.getProfile().getCivility().getBirthday() != null) {
+			Date newBirthday = userToUpdate.getProfile().getCivility().getBirthday();
+			currentUser.getProfile().getCivility().setBirthday(newBirthday);
+		}
+
+		if (userToUpdate.getAccount().getPassword() != null) {
+			String newPassword = userToUpdate.getAccount().getPassword();
+			currentUser.getAccount().setPassword(newPassword);
+		}
+
+		if (userToUpdate.getProfile().getAdress().getStreet() != null) {
+			String newStreet = userToUpdate.getProfile().getAdress().getStreet();
+			currentUser.getProfile().getAdress().setStreet(newStreet);
+		}
+
+		if (userToUpdate.getProfile().getAdress().getZipCode() != 0) {
+			int newZipCode = userToUpdate.getProfile().getAdress().getZipCode();
+			currentUser.getProfile().getAdress().setZipCode(newZipCode);
+		}
+		if (userToUpdate.getProfile().getAdress().getStreet() != null) {
+			String newStreet = userToUpdate.getProfile().getAdress().getCity();
+			currentUser.getProfile().getAdress().setStreet(newStreet);
+		}
+		userDao.updateUser(userToUpdate);
+
 	}
 
 }
